5b4c79e769cb83f0c34de17f6d329abae1cb6e14
tobiadefami <tobiadefami@gmail.com>
2022-10-11 16:59:41 +0100

    install

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup 
+
+setup(
+        name = "docmodel",
+        version = "1.0",
+        install_requires = [line for line in open("requirements/requirements.txt", "r", encoding="utf-8")],
+
+        )
