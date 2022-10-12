5fdd7103fe412b8046c35f7b8ed4b3f2ecc53b7f
tobiadefami <tobiadefami@gmail.com>
2022-10-12 12:53:46 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
         name = "docmodel",
         version = "1.0",
         install_requires = [line for line in open("requirements/requirements.txt", "r", encoding="utf-8")],
-
+        license = "MPL-2.0"
         )
